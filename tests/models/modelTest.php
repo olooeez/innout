4b15b0a95858a2d2137111768b21c1a70ca2f668
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-05-17 11:57:40 -0300

    Add a simple base model test

---
+++ tests/models/modelTest.php
@@ -0,0 +1,23 @@
+<?php
+require_once(dirname(__FILE__) . '/../../src/config/config.php');
+
+require_once(MODEL_PATH . '/model.php');
+
+use PHPUnit\Framework\TestCase;
+
+class modelTest extends TestCase {
+    public function testLoadUserFromArray() {
+        $user = [
+            'id' => 1,
+            'name' => 'John Doe',
+            'email' => 'john@example.com'
+        ];
+        
+        $model = new Model($user);
+        
+        $this->assertEquals($user['id'], $model->id);
+        $this->assertEquals($user['name'], $model->name);
+        $this->assertEquals($user['email'], $model->email);
+    }
+}
+?>
