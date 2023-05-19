fab87f5af693599db42625da856f4704665f9ca5
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-05-19 14:15:19 -0300

    Create login page

--- public/index.php
+++ public/index.php
@@ -1,23 +1,5 @@
 <?php
     require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-    require_once(MODEL_PATH . '/user.php');
+    require_once(VIEW_PATH . '/login.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>In-N-out</title>
-</head>
-<body>
-    <?php
-        foreach (User::get([], 'name') as $user) {
-            echo $user->name;
-            echo '<br>';
-        }
-    ?>
-</body>
-</html>
--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,7 @@ date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8');
 
 define('MODEL_PATH', dirname(realpath(__FILE__)) . '/../models');
+define('VIEW_PATH', dirname(realpath(__FILE__)) . '/../views');
 
 require_once(realpath(dirname(__FILE__) . '/database.php'));
 
