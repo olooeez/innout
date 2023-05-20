e1530e7d83f7d1126e57a07abbf0c036f7470b2f
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-05-20 11:24:00 -0300

    Create login controller

---
+++ src/controllers/login.php
@@ -0,0 +1,19 @@
+<?php
+
+loadModel('login');
+
+$exception = null;
+
+if (count($_POST) > 0) {
+    $login = new Login($_POST);
+
+    try {
+        $user = $login->checkLogin();
+    } catch (AppException $e) {
+        $exception = $e;
+    }
+}
+
+loadView('login', $_POST + ['exception' => $exception]);
+
+?>
---
+++ src/exceptions/ValidationException.php
@@ -0,0 +1,20 @@
+<?php
+
+class ValidationException extends AppException {
+    private $errors = [];
+
+    function __construct($errors = [], $message = 'Erros de validação.', $code = 0, $previous = null) {
+        parent::__construct($message, $code, $previous);
+        $this->errors = $errors;
+    }
+
+    public function getErrors() {
+        return $this->errors;
+    }
+
+    public function __get($attribute) {
+        return $this->errors[$attribute];
+    }
+}
+
+?>
